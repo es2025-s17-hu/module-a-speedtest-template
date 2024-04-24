43e01e301603e9fd0f7c5db025ab71281f33a7d1
Zoltan Sisak <zoltan.sisak@gmail.com>
2024-04-24 12:29:10 +0200

    task 2 update

--- task-2/index.js
+++ task-2/index.js
@@ -1,33 +1,36 @@
 const exampleData1 = [
-    {
-      items: [
-        { name: "Grilled chicken", price: 1200 },
-        { name: "French fries", price: 600 },
-        { name: "Coca-Cola", price: 400 },
-      ],
-    },
-    {
-      items: [
-        { name: "Grilled chicken", price: 1200 },
-        { name: "Coca-Cola", price: 400 },
-      ],
-    },
-    {
-      items: [
-        { name: "French fries", price: 600 },
-        { name: "Coca-Cola", price: 400 },
-      ],
-    },
-  ];
+  {
+    items: [
+      { name: "Grilled chicken", price: 1200 },
+      { name: "French fries", price: 600 },
+      { name: "Coca-Cola", price: 400 },
+    ],
+  },
+  {
+    items: [
+      { name: "Grilled chicken", price: 1200 },
+      { name: "Coca-Cola", price: 400 },
+    ],
+  },
+  {
+    items: [
+      { name: "French fries", price: 600 },
+      { name: "Coca-Cola", price: 400 },
+    ],
+  },
+];
 
-document.getElementById("example1").innerHTML += JSON.stringify(createReceipt(exampleData1), null, 2);
+document.getElementById("example1").innerHTML += JSON.stringify(
+  getStatistics(exampleData1),
+  null,
+  2
+);
 
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    fetch('getStatistics.js')
-        .then(response => response.text())
-        .then(text => {
-            document.getElementById('sourceCode').textContent = text;
-        })
-        .catch(err => console.error('Failed to load source code:', err));
+document.addEventListener("DOMContentLoaded", (event) => {
+  fetch("getStatistics.js")
+    .then((response) => response.text())
+    .then((text) => {
+      document.getElementById("sourceCode").textContent = text;
+    })
+    .catch((err) => console.error("Failed to load source code:", err));
 });
